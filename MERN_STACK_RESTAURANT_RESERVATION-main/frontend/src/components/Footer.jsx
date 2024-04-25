b7e65c726a965fe89956121d838b35b3192d2a22
Rutuja Gunjal <96534793+Rutuja520@users.noreply.github.com>
2024-04-25 21:59:27 +0530

    first commit

---
+++ MERN_STACK_RESTAURANT_RESERVATION-main/frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+          <div className="left">Mini Project by Rutuja Gunjal</div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
